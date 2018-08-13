64ab9eb5e4c4d91cc5ef604939ff595277887482
eugeny <eberkunsky@gmail.com>
2018-08-13 20:09:36 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public final static double EPS = 1e-6;
+
+    private double a = 2.8;
+    private double b = -0.3;
+    private double c = 4.0;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public double f(double x) {
+        if (x<1.4-EPS) {
+            return a*x*x+b*x+c;
+        } else if (x>1.4+EPS) {
+            return (a+b*x)/Math.sqrt(x*x+1);
+        } else {
+            return a/x+Math.sqrt(x*x+1);
+        }
+    }
+
+    public int steps(double start, double finish, double step) {
+        return (int) ((finish-start)/step+1);
+    }
+
+    public double[] fillX(double start, double finish, double step) {
+        return IntStream
+                .iterate(0, i->i+1)
+                .mapToDouble(i->start+i*step)
+                .limit(steps(start,finish,step))
+                .toArray();
+    }
+
+    public double[] fillY(double[] x) {
+        return Arrays.stream(x)
+                .map(this::f)
+                .toArray();
+    }
+}
---
+++ test/main/MainTest.java
@@ -0,0 +1,77 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private final static double EPS = 1e-6;
+
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = new Main();
+    }
+
+    @Test
+    void testF1() {
+        double x = 1;
+        double expected = 6.5;
+        double result = main.f(x);
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testF2() {
+        double x = 2;
+        double expected = 0.98386991;
+        double result = main.f(x);
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testF3() {
+        double x = 1.4;
+        double result = main.f(x);
+        double expected = 3.720465053;
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testSteps() {
+        double start = 0;
+        double finish = 10;
+        double step = 0.1;
+        int expected = 101;
+        int result = main.steps(start, finish, step);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFillX() {
+        double start = 0;
+        double finish = 2;
+        double step = 0.002;
+        double[] expected = {0, 1.4, 2};
+        double[] result = main.fillX(start, finish, step);
+        assertEquals(expected[0], result[0], EPS);
+        assertEquals(expected[1], result[700], EPS);
+        assertEquals(expected[2], result[1000], EPS);
+    }
+
+    @Test
+    void testFillY() {
+        double start = 0;
+        double finish = 2;
+        double step = 0.002;
+        double[] x = main.fillX(start, finish, step);
+        double[] expected = {4.0, 3.720465053, 0.98386991};
+        double[] result = main.fillY(x);
+        assertEquals(expected[0], result[0], EPS);
+        assertEquals(expected[1], result[700], EPS);
+        assertEquals(expected[2], result[1000], EPS);
+    }
+}
